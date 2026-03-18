e526af31e8a9bf7a10df5e25acefb579c02a5015
team0243 <76931176+team0243@users.noreply.github.com>
2026-03-18 16:54:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G8QD9T6X2X"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+
 
   gtag('config', 'G-G8QD9T6X2X');
 </script>
